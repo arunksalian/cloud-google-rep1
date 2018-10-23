02004e9455b99656c58f0bd3ea155e27156d82fa
svijayakumar <svijayakumar@ghx.com>
2018-10-23 13:06:07 +0530

    added save method

--- doorkeeper/src/main/java/org/slin/app/doorkeeper/controller/HomeController.java
+++ doorkeeper/src/main/java/org/slin/app/doorkeeper/controller/HomeController.java
@@ -1,5 +1,8 @@
 package org.slin.app.doorkeeper.controller;
 
+import org.slin.app.doorkeeper.entity.News;
+import org.slin.app.doorkeeper.repositories.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    @Autowired
+    private NewsRepository newsRepository;
+
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public String getHome () {
-        return "home";
+        News news = new News();
+
+        newsRepository.save(news);
+        return "home:".concat(String.valueOf(System.currentTimeMillis()));
     }
 }
--- doorkeeper/src/main/java/org/slin/app/doorkeeper/repositories/NewsRepository.java
+++ doorkeeper/src/main/java/org/slin/app/doorkeeper/repositories/NewsRepository.java
@@ -1,8 +1,9 @@
 package org.slin.app.doorkeeper.repositories;
 
+import org.slin.app.doorkeeper.entity.News;
 import org.slin.app.doorkeeper.service.bean.MessageBean;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NewsRepository extends MongoRepository<MessageBean, String> {
+public interface NewsRepository extends MongoRepository<News, String> {
 
 }
